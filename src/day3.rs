7b9297fb80003e12dacc874683ab852bdc1e06dd
Jay Malhotra <5047192+SapiensAnatis@users.noreply.github.com>
2023-12-03 19:24:26 +0000

    Day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::ops::Range;
 
 use regex::Regex;
@@ -10,16 +11,44 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let result = crate::day3::part1();
+        let result = part1();
+        println!("Final result: {}", result)
+    }
+
+    #[test]
+    fn test_part2() {
+        let result = part2();
         println!("Final result: {}", result)
     }
 
     #[test]
     fn test_get_adjacent_squares() {
-        assert_eq!(get_adjacent_squares(1, 1), &[(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]);
-        assert_eq!(get_adjacent_squares(0, 0), &[(0, 0), (0, 1), (1, 0), (1, 1)]);
-        assert_eq!(get_adjacent_squares(1, 0), &[(0, 0), (0, 1), (1, 0), (1, 1), (2, 0), (2, 1)]);
-        assert_eq!(get_adjacent_squares(0, 1), &[(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)])
+        assert_eq!(
+            get_adjacent_squares(1, 1),
+            &[
+                (0, 0),
+                (0, 1),
+                (0, 2),
+                (1, 0),
+                (1, 1),
+                (1, 2),
+                (2, 0),
+                (2, 1),
+                (2, 2)
+            ]
+        );
+        assert_eq!(
+            get_adjacent_squares(0, 0),
+            &[(0, 0), (0, 1), (1, 0), (1, 1)]
+        );
+        assert_eq!(
+            get_adjacent_squares(1, 0),
+            &[(0, 0), (0, 1), (1, 0), (1, 1), (2, 0), (2, 1)]
+        );
+        assert_eq!(
+            get_adjacent_squares(0, 1),
+            &[(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)]
+        )
     }
 }
 
@@ -32,23 +61,64 @@ fn part1() -> i32 {
         .collect();
 
     let mut sum = 0;
-    let mut line_no: usize = 0;
     let num_regex = Regex::new(r"\d+").unwrap();
 
-    for line in input.split('\n') {
-        let ranges: Vec<_> = num_regex.find_iter(&line).map(|m| (m.range(), m.as_str())).collect();
+    for (line_no, line) in input.split('\n').enumerate() {
+        let result: i32 = num_regex
+            .find_iter(&line)
+            .filter_map(|m| match validate_number(line_no, m.range(), &grid) {
+                true => Some(m.as_str()),
+                false => None,
+            })
+            .map(|m| m.parse::<i32>().unwrap())
+            .sum();
 
-        for range in ranges {
-            if validate_number(line_no, range.0, &grid) {
-                println!("{} was a part number", range.1);
-                sum += range.1.parse::<i32>().unwrap();
-            }
-        }
-
-        line_no += 1;
+        sum += result;
     }
 
     return sum;
+}
+
+fn part2() -> i32 {
+    let input = util::read_real(3);
+
+    let mut sum = 0;
+
+    let grid: Vec<Vec<char>> = input
+        .split('\n')
+        .map(|line| line.chars().collect())
+        .collect();
+
+    let num_regex = Regex::new(r"\d+").unwrap();
+
+    let it = input
+        .split('\n')
+        .enumerate()
+        .flat_map(|(line_no, line)|
+            num_regex
+                .find_iter(line)
+                .filter_map(|m| match get_gear(line_no, m.range(), &grid) {
+                    Some(gear_coord) => Some(GearResult {
+                        number: m.as_str().parse::<i32>().unwrap(),
+                        gear_coord,
+                    }),
+                    None => None,
+                }).collect::<Vec<GearResult>>());
+
+    let mut coord_hashmap = HashMap::new();
+
+    for result in it {
+        let entry = coord_hashmap.entry(result.gear_coord).or_insert(Vec::new());
+        entry.push(result.number);
+    }
+
+    for (_, gears) in coord_hashmap {
+        if gears.len() > 1 {
+            sum += gears[0] * gears[1];
+        }
+    }
+
+    sum
 }
 
 fn validate_number(row: usize, range: Range<usize>, grid: &Vec<Vec<char>>) -> bool {
@@ -76,21 +146,62 @@ fn validate_number(row: usize, range: Range<usize>, grid: &Vec<Vec<char>>) -> bo
     false
 }
 
+fn get_gear(row: usize, range: Range<usize>, grid: &Vec<Vec<char>>) -> Option<Coordinate> {
+    let col_len = grid[0].len();
+
+    for i in range.start..range.end {
+        let squares = get_adjacent_squares(row, i);
+        for (j_row, j_col) in squares {
+            // println!("Checking adjacent {}, {}", j_row, j_col);
+
+            if j_row >= grid.len() || j_col >= col_len {
+                continue;
+            }
+
+            let char = grid[j_row][j_col];
+
+            if char == '*' {
+                return Some(Coordinate {
+                    row: j_row,
+                    col: j_col,
+                });
+            }
+        }
+    }
+
+    None
+}
+
 fn get_adjacent_squares(row: usize, col: usize) -> Vec<(usize, usize)> {
     let mut result = Vec::new();
     let mut row_start = row;
     let mut col_start = col;
 
-    if row_start > 0 { row_start -= 1; }
-    if col_start > 0 { col_start -= 1; }
+    if row_start > 0 {
+        row_start -= 1;
+    }
+    if col_start > 0 {
+        col_start -= 1;
+    }
 
     for curr_row in row_start..=row + 1 {
         for curr_col in col_start..=col + 1 {
-            if curr_row >= 0 && curr_col >= 0 {
-                result.push((curr_row, curr_col))
-            }
+            result.push((curr_row, curr_col))
         }
     }
 
     result
+}
+
+#[derive(PartialEq, Debug, Eq, Hash)]
+struct Coordinate {
+    row: usize,
+    col: usize,
+}
+
+
+#[derive(Debug)]
+struct GearResult {
+    number: i32,
+    gear_coord: Coordinate,
 }
